8273d344260b9cfd1b32bef995f6856ed3084eec
Thomas Wilhem <ThomasWilhem@outlook.fr>
2023-04-11 14:57:08 +0200

    feat: reservation added

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :reservations
   get 'flights/index'
   get 'flights/show'
   get 'flights/new'
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_11_122622) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_11_124520) do
   create_table "flights", force: :cascade do |t|
     t.string "number"
     t.string "departure_airport"
@@ -21,6 +21,16 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_11_122622) do
     t.integer "duration"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "reservations", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.integer "flight_id", null: false
+    t.integer "count"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["flight_id"], name: "index_reservations_on_flight_id"
+    t.index ["user_id"], name: "index_reservations_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -35,4 +45,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_11_122622) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "reservations", "flights"
+  add_foreign_key "reservations", "users"
 end
