695d59c1fb409716eb81652de415c535c1d6949c
Thomas Wilhem <thomaswilhempro@outlook.fr>
2023-05-09 12:44:37 +0200

    fix: reservation working

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,7 @@
 Rails.application.routes.draw do
-  resources :reservations, only: [:index, :show, :new, :create]
-  get 'flights/index'
-  get 'flights/show'
-  get 'flights/new'
-  get 'flights/edit'
+  resources :reservations, only: [:index, :show, :new, :create, :edit]
   resources :flights
 
   devise_for :users
   root 'application#index'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
 end
